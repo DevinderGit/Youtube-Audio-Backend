b819445ca8b347241e778730e1afa83f4a689068
DevinderSingh <sdevider@gmail.com>
2025-04-27 20:34:45 +0530

    f

--- server.js
+++ server.js
@@ -55,7 +55,8 @@ app.get('/stream', async (req, res) => {
 
     try {
         const ytDlp = spawn('yt-dlp', [
-            '--add-header', `cookie: ${globalCookies}`,
+            '--add-header', 
+            '--cookies', 'cookies.txt',
             '--user-agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/120.0.0.0 Safari/537.36',
             '-f', 'best[height<=360]',
             '-o', '-', 
